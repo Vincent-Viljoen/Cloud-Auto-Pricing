7387aecb9d46abb5dd57ea28fa6ca5927685dfbd
AFRICA\vincent.viljoen <vincent.viljoen@dimensiondata.com>
2022-06-29 13:12:11 +0200

    Added structure

--- autoPricing.py
+++ autoPricing.py
@@ -2,6 +2,14 @@ import pandas as pd
 import numpy as np
 import requests
 
+#Choose correct vm
+
+
+#Select general disk for vm
+
+
+#Get correct disks
+
 #Making api request and parsing it to json format
 r = requests.get(url = "https://prices.azure.com/api/retail/prices?$filter=meterId  eq 'ff595cb7-60f8-4815-8022-adee24f52953'")
 data = r.json()
