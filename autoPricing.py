6956d1187b3cc6791ab19772883648e4fb85ab58
AFRICA\vincent.viljoen <vincent.viljoen@dimensiondata.com>
2022-06-29 10:18:11 +0200

    Seeing if it works

--- autoPricing.py
+++ autoPricing.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import numpy as np
+import requests
 
-x = pd.DataFrame()
-
-print('test 2')
-
-print(x)
+#Making api request and parsing it to json format
+r = requests.get(url = "https://prices.azure.com/api/retail/prices?$filter=meterId  eq 'ff595cb7-60f8-4815-8022-adee24f52953'")
+data = r.json()
+print(data)
