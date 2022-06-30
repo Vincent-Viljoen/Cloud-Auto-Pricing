3c93e315d42ef6c7d94c97e24f79ac38d50ceec2
Vincent-Viljoen <vincentviljoen23@gmail.com>
2022-06-30 09:18:22 +0200

    Changes commit

--- autoPricing.py
+++ autoPricing.py
@@ -3,9 +3,9 @@ import numpy as np
 import requests
 
 #import data
-
 df = pd.read_excel('Bryte costing.xlsx', sheet_name = 'Sheet4')
-vmSpecs = pd.read_excel('Avaliable VMs in SA North.xlsx')
+#Remove first row, it is blank
+vmSpecs = pd.read_excel('Avaliable VMs in SA North.xlsx').iloc[1:,]
 
 #Add series to skuNAME
 for i in range(len(df)):
@@ -19,6 +19,8 @@ for i in range(len(df)):
     else:
         skuNAME = 'E'
 
+print(vmSpecs)
+
 #Link vm IDs with vm Name, CPUs and RAM
 
 #Choose correct vm
@@ -26,11 +28,11 @@ for i in range(len(df)):
 #Make api request and parsing it to json format
 
 #vmId needs to be dynamic bases on the requirements
-skuNAME = "'F2'"
-vmID = "'dc7ab2e8-27b1-4b96-b55b-f0b09f5f68a2'"
-vmURL = f"https://prices.azure.com/api/retail/prices?$filter=meterId  eq {vmID} and SkuName eq {skuNAME}"
-r = requests.get(url = vmURL)
-data = r.json()
+# skuNAME = "'F2'"
+# vmID = "'dc7ab2e8-27b1-4b96-b55b-f0b09f5f68a2'"
+# vmURL = f"https://prices.azure.com/api/retail/prices?$filter=meterId  eq {vmID} and SkuName eq {skuNAME}"
+# r = requests.get(url = vmURL)
+# data = r.json()
 
 #Select general disk for vm
 
